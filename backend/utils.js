26c873bf65d858227f4907e2084fbf30fc1773e3
azukiazusa1 <cycleippei@gmail.com>
2021-01-17 17:38:21 +0900

    auth middleware

--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import { restart } from 'nodemon'
 import config from './config'
 
 export const getToken = (user) => {
@@ -10,4 +11,34 @@ export const getToken = (user) => {
   }, config.JWT_SECRET, {
     expiresIn: '48h'
   })
+}
+
+export const isAuth = (req, res, next) => {
+  const token = req.headers.authorization
+  if (token) {
+    const onlyToken = token.slice(7, token.length)
+    jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({
+          msg: 'Invalid Token'
+        })
+      }
+      req.user = token
+      next()
+      return
+    })
+  }
+  return res.status(401).send({
+    msg: 'Token is not supplied.'
+  })
+}
+
+export const isAdmin = (req, res, next) => {
+  if (rea.user && req.user.isAdmin) {
+    return next()
+  } else {
+    return res.status(401).send({
+      msg: 'Admin Token is not valid'
+    })
+  }
 }
